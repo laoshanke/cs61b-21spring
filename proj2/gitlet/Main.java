56369f6b1077096a6c2b398a2e983f9a3c1d0946
laoshanke <3276390488@qq.com>
2025-04-21 16:31:36 +0800

    init

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,5 +1,7 @@
 package gitlet;
 
+import static gitlet.Repository.GITLET_DIR;
+
 /** Driver class for Gitlet, a subset of the Git version-control system.
  *  @author TODO
  */
@@ -10,15 +12,36 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
+        if(args.length == 0) {
+            System.out.println("Please enter a command.");
+            System.exit(0);
+        }
+        if(args[0].getClass() != String.class) {
+            System.out.println("Incorrect operands.");
+            System.exit(0);
+        }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
-                // TODO: handle the `init` command
+                if(args.length != 1) {
+                    System.out.println("Incorrect operands.");
+                    System.exit(0);
+                }
+                Repository repo = new Repository();
+                repo.init();
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
             // TODO: FILL THE REST IN
+            default:
+                System.out.println("No command with that name exists.");
+        }
+    }
+    void initExam(){
+        if (!GITLET_DIR.exists()) {
+            System.out.println("Not in an initialized Gitlet directory.");
+            System.exit(0);
         }
     }
 }
